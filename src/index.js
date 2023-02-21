d51b137138a8930194ebb87ba74c959047828432
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2023-02-21 22:29:47 +0700

    [add] button setup for face changes from expression preset

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const { normal_smile } = require('./faces')
+const { expresionPreset } = require('./data/faces')
 
 const LED_PIN = D4
 const LED_COUNT = 154 // Rina board leds
 const RINA_COLOR = [255, 20, 147]
 
-const rgb = new Uint8ClampedArray(LED_COUNT * 3) // Forces the values to be between 0 and 255
+let rgb = new Uint8ClampedArray(LED_COUNT * 3) // Forces the values to be between 0 and 255
 let brightness = 0.2 // 0 to 1 based on how bright we want the colours to be.
 
+let active_eye = 0
+let active_blush = false
+let active_mouth = 0
+
+let prev_active_eye = null
+let prev_active_blush = null
+let prev_active_mouth = null
+
 pinMode(D16, 'input_pulldown')
+pinMode(D17, 'input_pulldown')
+pinMode(D18, 'input_pulldown')
+pinMode(D19, 'input_pulldown')
 
 setWatch(
   function (e) {
@@ -19,6 +30,44 @@ setWatch(
     setLed()
   },
   D16,
+  { repeat: true, edge: 'rising', debounce: 50 }
+)
+setWatch(
+  function (e) {
+    if (e.time - e.lastTime < 0.2) return
+
+    prev_active_eye = active_eye
+    active_eye = (active_eye + 1) % expresionPreset.eyes.length
+
+    setLed()
+  },
+  D17,
+  { repeat: true, edge: 'rising' }
+)
+setWatch(
+  function (e) {
+    if (e.time - e.lastTime < 0.2) return
+
+    prev_active_blush = active_blush
+    if (active_blush >= expresionPreset.blush.length - 1) active_blush = false
+    else active_blush = typeof active_blush === 'number' ? (active_blush || 0) + 1 : 0
+
+    setLed()
+  },
+  D18,
+  { repeat: true, edge: 'rising' }
+)
+setWatch(
+  function (e) {
+    console.log('change mouth', brightness, e.time - e.lastTime)
+    if (e.time - e.lastTime < 0.2) return
+
+    prev_active_mouth = active_mouth
+    active_mouth = (active_mouth + 1) % expresionPreset.mouth.length
+
+    setLed()
+  },
+  D19,
   { repeat: true, edge: 'rising' }
 )
 
@@ -34,8 +83,27 @@ function draw() {
 }
 
 function setLed() {
-  normal_smile.forEach((num) => setColor(num, RINA_COLOR))
+  clearLed()
+
+  if (active_eye !== prev_active_eye) {
+    expresionPreset.eyes[active_eye].data.forEach((num) => setColor(num, RINA_COLOR))
+  }
+  if (active_mouth !== prev_active_mouth) {
+    expresionPreset.mouth[active_mouth].data.forEach((num) => setColor(num, RINA_COLOR))
+  }
+  if (active_blush !== prev_active_blush) {
+    if (typeof active_blush === 'number' && expresionPreset.eyes[active_eye].withBlush) {
+      expresionPreset.blush[active_blush].data.forEach((num) => setColor(num, RINA_COLOR))
+    } else {
+      active_blush = false
+    }
+  }
+
   draw()
+}
+
+function clearLed() {
+  rgb = new Uint8ClampedArray(LED_COUNT * 3)
 }
 
 setLed()
